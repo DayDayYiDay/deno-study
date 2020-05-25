742f070bf588b37a512a42e4ddb2e43a8d5ff790
DayYiDayDay <cscfgogogo@gmail.com>
2020-05-25 21:52:44 +0800

    cache relative import

--- util.ts
+++ util.ts
@@ -6,7 +6,8 @@
 export const globalEval = eval;
 
 // A reference to the global object.
-const _global = globalEval("this");
+// TODO The underscore is because it's conflicting with @types/node.
+export const _global = globalEval("this");
 
 const print = V8Worker2.print;
 
@@ -49,6 +50,6 @@ function stringifyArgs(args: any[]): string {
 
 export function assert(cond: boolean, msg = "") {
   if (!cond) {
-    throw Error("Assertion failed. " + msg);
+    throw Error("Assert fail. " + msg);
   }
 }
